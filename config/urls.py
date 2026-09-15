02b5d98aa5148fe2d48b3af5c5292fa661d4a02b
LucasVidela7 <40281222+LucasVidela7@users.noreply.github.com>
2026-09-14 21:30:03 -0300

    Agregar login y logout

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
+
 urlpatterns = [
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="registration/login.html",
+            redirect_authenticated_user=True,
+        ),
+        name="login",
+    ),
+
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(),
+        name="logout",
+    ),
 
     path(
         "",
@@ -27,9 +43,14 @@ urlpatterns = [
         "produccion/",
         include("produccion.urls"),
     ),
+
     path(
         "clientes/",
-        include("clientes.urls")
+        include("clientes.urls"),
     ),
-    path("calculadora/", include("calculadora.urls")),
+
+    path(
+        "calculadora/",
+        include("calculadora.urls"),
+    ),
 ]
