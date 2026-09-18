0ad82a1e9272294a1e878f53675cb50c82f03bb4
LucasVidela7 <40281222+LucasVidela7@users.noreply.github.com>
2026-09-17 21:10:16 -0300

    QA: validar acciones reales en historial de cliente

--- clientes/tests.py
+++ clientes/tests.py
@@ -48,10 +48,15 @@ class DetalleClienteAccionesPedidoTests(TestCase):
     def test_pendiente_muestra_editar_cancelar_y_eliminar(self):
         respuesta = self.client.get(self.url)
 
-        self.assertContains(respuesta, "btn-editar-pedido")
-        self.assertContains(respuesta, "btn-cancelar-pedido")
-        self.assertContains(respuesta, "btn-eliminar-pedido")
-        self.assertNotContains(respuesta, "btn-entregar-pedido")
+        editar = reverse("pedidos:editar", args=[self.pedido.id])
+        cancelar = reverse("pedidos:cancelar", args=[self.pedido.id])
+        eliminar = reverse("pedidos:eliminar", args=[self.pedido.id])
+        entregar = reverse("pedidos:entregar", args=[self.pedido.id])
+
+        self.assertContains(respuesta, f'href="{editar}?')
+        self.assertContains(respuesta, f'action="{cancelar}"')
+        self.assertContains(respuesta, f'action="{eliminar}"')
+        self.assertNotContains(respuesta, f'action="{entregar}"')
 
     def test_listo_muestra_entregar_y_oculta_edicion(self):
         self.pedido.estado = "LISTO"
@@ -59,10 +64,15 @@ class DetalleClienteAccionesPedidoTests(TestCase):
 
         respuesta = self.client.get(self.url)
 
-        self.assertContains(respuesta, "btn-entregar-pedido")
-        self.assertNotContains(respuesta, "btn-editar-pedido")
-        self.assertNotContains(respuesta, "btn-cancelar-pedido")
-        self.assertNotContains(respuesta, "btn-eliminar-pedido")
+        editar = reverse("pedidos:editar", args=[self.pedido.id])
+        cancelar = reverse("pedidos:cancelar", args=[self.pedido.id])
+        eliminar = reverse("pedidos:eliminar", args=[self.pedido.id])
+        entregar = reverse("pedidos:entregar", args=[self.pedido.id])
+
+        self.assertContains(respuesta, f'action="{entregar}"')
+        self.assertNotContains(respuesta, f'href="{editar}?')
+        self.assertNotContains(respuesta, f'action="{cancelar}"')
+        self.assertNotContains(respuesta, f'action="{eliminar}"')
 
     def test_preparando_no_permite_acciones_de_modificacion(self):
         self.pedido.estado = "PREPARANDO"
@@ -70,10 +80,15 @@ class DetalleClienteAccionesPedidoTests(TestCase):
 
         respuesta = self.client.get(self.url)
 
-        self.assertNotContains(respuesta, "btn-editar-pedido")
-        self.assertNotContains(respuesta, "btn-cancelar-pedido")
-        self.assertNotContains(respuesta, "btn-eliminar-pedido")
-        self.assertNotContains(respuesta, "btn-entregar-pedido")
+        editar = reverse("pedidos:editar", args=[self.pedido.id])
+        cancelar = reverse("pedidos:cancelar", args=[self.pedido.id])
+        eliminar = reverse("pedidos:eliminar", args=[self.pedido.id])
+        entregar = reverse("pedidos:entregar", args=[self.pedido.id])
+
+        self.assertNotContains(respuesta, f'href="{editar}?')
+        self.assertNotContains(respuesta, f'action="{cancelar}"')
+        self.assertNotContains(respuesta, f'action="{eliminar}"')
+        self.assertNotContains(respuesta, f'action="{entregar}"')
 
     def test_detalle_abre_en_modo_consulta(self):
         respuesta = self.client.get(self.url)
