ced133d96e98a165fc2138d717204bab01b97427
LucasVidela7 <40281222+LucasVidela7@users.noreply.github.com>
2026-09-19 00:47:07 -0300

    QA: agregar rutas para Web Push

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -14,6 +14,24 @@ urlpatterns = [
     ),
 
     path(
+        "sw.js",
+        views.push_service_worker,
+        name="push_service_worker",
+    ),
+
+    path(
+        "push/suscribir/",
+        views.push_suscribir,
+        name="push_suscribir",
+    ),
+
+    path(
+        "push/desuscribir/",
+        views.push_desuscribir,
+        name="push_desuscribir",
+    ),
+
+    path(
         "dashboard/produccion/<int:produccion_id>/iniciar/",
         views.iniciar_produccion_dashboard,
         name="produccion_iniciar",
