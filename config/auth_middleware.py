5739f5918d83df0e7553ce34174727b833ef6319
LucasVidela7 <40281222+LucasVidela7@users.noreply.github.com>
2026-09-24 12:29:04 -0300

    Cleanup: retirar excepción temporal de sincronización

--- config/auth_middleware.py
+++ config/auth_middleware.py
@@ -31,7 +31,6 @@ class LoginRequiredMiddleware:
         "catalogo_arrepentimiento",
         "catalogo_arrepentimiento_gracias",
         "healthcheck",
-        "db_sync_export",
         "evento",
     }
 
