8195860cbc158d24b2c4ba08e1225f01225a947f
LucasVidela7 <40281222+LucasVidela7@users.noreply.github.com>
2026-09-24 12:00:04 -0300

    QA tests: mostrar nombre y HEX de color personalizado

--- productos/test_catalogo_publico.py
+++ productos/test_catalogo_publico.py
@@ -541,6 +541,34 @@ class CatalogoPublicoTests(TestCase):
         listado = self.client.get(reverse("catalogo_productos"))
         self.assertContains(listado, "Color a elección")
 
+    def test_producto_muestra_nombre_de_color_personalizado_y_su_hex(self):
+        config, _ = ConfiguracionCatalogo.objects.get_or_create(pk=1)
+        config.colores_disponibles = "Verde manzana|#12AB34"
+        config.save(update_fields=["colores_disponibles"])
+        self.producto.permite_elegir_color = True
+        self.producto.save(update_fields=["permite_elegir_color"])
+
+        response = self.client.get(
+            reverse(
+                "catalogo_producto_detalle",
+                args=[self.producto.id],
+            )
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(
+            response,
+            'data-color-value="Verde manzana"',
+        )
+        self.assertContains(
+            response,
+            'data-color-hex="#12AB34"',
+        )
+        self.assertContains(
+            response,
+            'aria-label="Verde manzana"',
+        )
+
     def test_kit_libre_muestra_adicional_por_mismo_color(self):
         config, _ = ConfiguracionCatalogo.objects.get_or_create(pk=1)
         config.colores_disponibles = "Rojo\nAzul"
